a8af54a925e25e0d984507e28811a92f602bc82c
NavagonStarlightManav <manavgoyal2006@gmail.com>
2025-03-11 10:06:46 +0530

    Learning Variables and Input Commands

---
+++ Journey_Day1.py
@@ -0,0 +1,31 @@
+
+# We can also add comments and can comment out/in  piece of line using ctrl +/
+
+print("Manav Goyal has started the engine of successful journey \nEven Stronger than ever\nReady to conquer")
+
+print("Will" + " " + "the" + " " +  "inner demon")
+
+input("What is your name ? ")
+
+print("Hello" + input(" what is your name ?") + " !")
+
+name="Manav"
+print(name)
+
+name="Goyal"
+print(name)
+
+username=input("Please enter your name sir ")
+length_1=len(name)
+print(length_1)
+
+print(len(input("What is your name mam ?")))
+
+length=len(input("What is your name officer ?"))
+print(" The length is "+ str(length))
+
+print("Welcome to the Band Name Generator.\n")
+name_city=input("What's the name of the city you grew in?\n")
+pet_name=input("What's your pet's name?\n")
+print("Your band name could be "+name_city+" "+pet_name)
+
