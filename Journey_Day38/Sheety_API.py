296528daf1ab4611e92e3a30dd92d134dcaf1ac4
NavagonStarlightManav <manavgoyal2006@gmail.com>
2025-08-15 19:45:26 +0530

    Using Google sheet as API
    Saving Results of NLP Model of Nutritionix API in Personal Google Sheet using Sheety API

---
+++ Journey_Day38/Sheety_API.py
@@ -0,0 +1,61 @@
+import requests
+import datetime
+import os
+from openpyxl import Workbook
+
+
+application_id = "dc01167d"
+application_key = "0b3c9311f757ac2aabeb06097f1267d4"
+nutrition_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+APP_ID = os.environ.get("application_id")
+print(APP_ID)
+
+nutrition_headers = {
+    "x-app-id": application_id,
+    "x-app-key": application_key
+}
+
+exercise_text = input("Tell me which exercises you did: ")
+
+
+user_param = {
+    "query": exercise_text,
+    "gender": "male",
+    "weight_kg": 75,
+    "height_cm": 180,
+    "age": 19
+}
+
+response = requests.post(url=nutrition_endpoint, headers=nutrition_headers, json=user_param)
+result = response.json()
+
+
+today_date = datetime.datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.datetime.now().strftime("%X")
+
+Sheet_endpoint="https://api.sheety.co/698eceba8cb124c9fa5db42221d59c68/manavPersonalWorkouts/workouts"
+
+bearer_headers = {
+        "Authorization": "Bearer Manav@3592 "
+    }
+
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    sheet_response = requests.post(
+        Sheet_endpoint,
+        json=sheet_inputs,
+        headers=bearer_headers
+    )
+    print(sheet_response.text)
+
+
